33d4b0beea8039973395fdb00a0e1432692f84b6
iammary <iammary@users.noreply.github.com>
2023-11-05 11:49:51 +0000

    Deploying to gh-pages from @ iammary/litel@d6497630e0159ceaaedfae80c8d33cf56c87998a 🚀

---
+++ 3/assets/preview-97c7835e.js
@@ -0,0 +1,2 @@
+const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/date$/i}}}};export{e as default};
+//# sourceMappingURL=preview-97c7835e.js.map
